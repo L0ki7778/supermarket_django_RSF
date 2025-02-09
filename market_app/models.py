523a68c6f6c566a587e6949f2843b458c789816c
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-02-09 04:54:55 +0100

    added models for market

--- market_app/models.py
+++ market_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Market(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    net_worth = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return f'The market {self.name}'
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    contact_info = models.TextField()
+    markets = models.ManyToManyField(Market, related_name="sellers")
+
+    def __str__(self):
+        return f'The Seller {self.name} bought in in {self.markets}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    market = models.ForeignKey(
+        Market, on_delete=models.CASCADE, related_name='Products')
+    seller = models.ForeignKey(
+        Seller, on_delete=models.CASCADE, related_name='Products')
+
+    def __str__(self):
+        return self.name
